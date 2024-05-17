b31a228ccdf255317ba5a3d124bc38b6f7084580
mustafaguler3 <mustafa.glr@hotmail.com>
2024-05-17 15:57:49 +0300

    create post and account service

---
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Role;
+import com.example.demo.model.User;
+
+import java.util.List;
+
+public interface AccountService {
+    void saveUser(User user);
+    User findByUsername(String username);
+    User findByEmail(String email);
+    List<User> userList();
+    Role findUserRoleByName(String role);
+    Role saveRole(Role role);
+    void updateUser(User user);
+    User findById(Long id);
+    void deleteUser(User user);
+    void resetPassword(User user);
+    List<User> getUserListByUsername(String username);
+    void simpleSave(User user);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Post;
+import com.example.demo.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PostService {
+    Post savePost(User user, Map<String,String> request,String postImageName);
+    List<Post> postList();
+    Post getPostById(Long id);
+    List<Post> findPostByUsername(String username);
+    Post deletePost(Post post);
+    String savePostImage(HttpServletRequest request,String fileName);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
